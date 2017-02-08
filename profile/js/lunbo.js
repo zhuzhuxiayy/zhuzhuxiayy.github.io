e160db2872275eaf46c211efcf79c6ee9cc1ef24
zhuzhuxiayy <1053059075@qq.com>
2017-02-08 12:34:53 +0800

    2code

--- profile/js/lunbo.js
+++ profile/js/lunbo.js
@@ -3,10 +3,10 @@ clearInterval(timer);
 var index = 3;
 
 function change() {
-    $('.col2-2 img').eq(index).css({ zIndex: '1' }).siblings().css({ zIndex: '0' });
-    $('#leftImg').css({ top: 115 * (3 - index) + 'px' });
     index--;
     if (index < 0) {
         index = 3;
     }
+    $('.col2-2 img').eq(index).css({ zIndex: '1' }).siblings().css({ zIndex: '0' });
+    $('#leftImg').css({ top: 115 * (3 - index) + 'px' });
 }
